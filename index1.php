900d92e4cef7cfa44c765c97e2fcbaef8625be1f
Kusec <121029904+Kusec@users.noreply.github.com>
2024-10-05 18:40:48 +0200

    Add files via upload

---
+++ index1.php
@@ -0,0 +1,23 @@
+<?php
+    //php blok
+?>
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset= UTF-8">
+        <meta name= "description" content="Da Vincijev kod je kriminalistički triler američkog pisca Dana Browna.">
+        <meta name= "keywords" content="Da Vincijev kod, Dan Brown, kriminalistički triler">
+        <meta name= "author" content= "Marko Kušec">
+        <meta name="viewport" content="width=width-device; initial-scale=1.0">
+
+        <title>Da vincijev kod</title>    
+    </head>
+    <body>
+    
+        <h1>Da Vincijev kod</h1>
+        <p>Da Vincijev kod je kriminalistički triler američkog pisca Dana Browna</p>
+        <a href="https://hr.wikipedia.org/wiki/Da_Vincijev_kod" target="_blank">Više o Da Vincijevom kodu</a>
+    </body>
+</html>
+<!-- index1.php -->
+
